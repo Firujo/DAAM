4e0d0c89f6e0f058f867700e06c2ac0318000d10
phillipsgoncalves <filisgoncalves@gmail.com>
2017-03-30 03:18:53 +0100

    Register validation

--- app/src/main/java/pt/iul/iscte/daam/fitmeet/RegisterActivity.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/RegisterActivity.java
@@ -19,11 +19,9 @@ public class RegisterActivity extends AppCompatActivity {
   private EditText passwordEditText;
   private EditText passwordConfirmationEditText;
 
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_register);
-
 
     setupToolbar();
 
@@ -46,7 +44,15 @@ public class RegisterActivity extends AppCompatActivity {
   private void setupListener() {
     registerButton.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
+        final String name = nameEditText.getText().toString();
+        final String username = usernameEditText.getText().toString();
+        final String password = passwordEditText.getText().toString();
+        final String passwordConfirmation = passwordConfirmationEditText.getText().toString();
 
+        if (validateRegister(name, username, password, passwordConfirmation)) {
+          //TODO : Taborda make regist call here.
+          tabordaTestRegister(name, username, password, passwordConfirmation);
+        }
       }
     });
   }
@@ -54,6 +60,30 @@ public class RegisterActivity extends AppCompatActivity {
   @Override public boolean onSupportNavigateUp() {
     onBackPressed();
     return true;
+  }
+
+  private boolean validateRegister(String name, String username, String password,
+      String passwordConfirmation) {//todo apply MVP + discuss verifications
+    if (name.isEmpty()) {
+      return false;
+    } else if (username.isEmpty()) {
+      return false;
+    } else if (password.isEmpty()) {
+      return false;
+    } else if (passwordConfirmation.isEmpty()) {
+      return false;
+    } else if (!password.equals(passwordConfirmation)) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Metodo po Xôr Bruno Taborda testar o registo
+   */
+  private void tabordaTestRegister(String name, String username, String password,
+      String passwordConfirmation) {
+    //// TODO: Taborda test registo here
   }
 
 }
