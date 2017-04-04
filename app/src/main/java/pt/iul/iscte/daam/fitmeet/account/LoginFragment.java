ed29350b8eec05bc3eabc4b5c98c210a72463815
Bruno <brunotaborda1995@hotmail.com>
2017-04-04 23:17:56 +0100

    Login email/password
    Criado e testado login via email/password com acesso ao firebase authentication.

--- app/src/main/java/pt/iul/iscte/daam/fitmeet/account/LoginFragment.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/account/LoginFragment.java
@@ -2,6 +2,7 @@ package pt.iul.iscte.daam.fitmeet.account;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,14 @@ import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import pt.iul.iscte.daam.fitmeet.R;
 
 public class LoginFragment extends Fragment {
@@ -16,6 +25,9 @@ public class LoginFragment extends Fragment {
   private Button loginButton;
   private EditText usernameEditText;
   private EditText passwordEditText;
+
+  private FirebaseAuth mAuth;
+  private FirebaseAuth.AuthStateListener mAuthListener;
 
   private OnFragmentInteractionListener mListener;
 
@@ -29,6 +41,20 @@ public class LoginFragment extends Fragment {
 
 
   @Override public void onCreate(Bundle savedInstanceState) {
+
+    mAuth = FirebaseAuth.getInstance();
+
+    mAuthListener = new FirebaseAuth.AuthStateListener() {
+      @Override
+      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+          System.out.println("onAuthStateChanged:signed_in:" + user.getUid());
+        } else {
+          System.out.println("onAuthStateChanged:signed_out");
+        }
+      }
+    };
     super.onCreate(savedInstanceState);
   }
 
@@ -42,6 +68,20 @@ public class LoginFragment extends Fragment {
 
     setupListeners();
     return view;
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    mAuth.addAuthStateListener(mAuthListener);
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (mAuthListener != null) {
+      mAuth.removeAuthStateListener(mAuthListener);
+    }
   }
 
   private void setupListeners() {
@@ -71,6 +111,29 @@ public class LoginFragment extends Fragment {
   private void tabordaLoginTeste(String username, String password) {
     //todo taborda test here
     System.out.println("testing login with : " + username + " ; " + password);
+
+    mAuth.signInWithEmailAndPassword(username, password)
+            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> task) {
+                System.out.println("signInWithEmail:onComplete:" + task.isSuccessful());
+
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                  System.out.println("signInWithEmail:failed" + task.getException());
+                  Toast.makeText(getActivity(), "Email or password is invalid",
+                          Toast.LENGTH_SHORT).show();
+                }
+                else{
+                  Toast.makeText(getActivity(), "Login com sucesso",
+                          Toast.LENGTH_SHORT).show();
+                }
+
+                // ...
+              }
+            });
   }
 
   public interface OnFragmentInteractionListener {
