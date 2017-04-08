f0b17a68fd79cd978644b8f05c6865e936981d7d
jdandrade <joao.andrade@aptoide.com>
2017-04-08 16:02:37 +0100

    Facebook Account implementation.

---
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/account/FacebookAccount.java
@@ -0,0 +1,41 @@
+package pt.iul.iscte.daam.fitmeet.account;
+
+import com.facebook.login.LoginManager;
+import rx.Completable;
+
+public class FacebookAccount implements Account {
+
+  private final Account account;
+
+  public FacebookAccount(Account account) {
+    this.account = account;
+  }
+
+  @Override public Completable logout() {
+    return Completable.fromAction(() -> LoginManager.getInstance().logOut());
+  }
+
+  @Override public String getId() {
+    return account.getId();
+  }
+
+  @Override public String getNickname() {
+    return account.getNickname();
+  }
+
+  @Override public String getAvatar() {
+    return account.getAvatar();
+  }
+
+  @Override public boolean isLoggedIn() {
+    return account.isLoggedIn();
+  }
+
+  @Override public String getEmail() {
+    return account.getEmail();
+  }
+
+  @Override public String getPassword() {
+    return account.getPassword();
+  }
+}
