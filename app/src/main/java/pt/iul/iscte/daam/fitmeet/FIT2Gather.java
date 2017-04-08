3145df322edd0c001d764778244b15a8001f5276
jdandrade <joao.andrade@aptoide.com>
2017-04-08 16:22:12 +0100

    Created application "component" that will hold reference to the user account.

---
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/FIT2Gather.java
@@ -0,0 +1,12 @@
+package pt.iul.iscte.daam.fitmeet;
+
+/**
+ * Created by jdandrade on 08/04/2017.
+ */
+
+public class FIT2Gather extends android.app.Application {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+
+}
