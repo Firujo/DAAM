90bf16191a079a8505a8cc8f718255535e3f280a
Bruno Taborda <bmtta@iscte.pt>
2017-04-11 21:55:36 +0100

    Read data
    Adicionado o listener que lê os dados da database.

--- app/src/main/java/pt/iul/iscte/daam/fitmeet/data/FireBDatabase.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/data/FireBDatabase.java
@@ -1,7 +1,11 @@
 package pt.iul.iscte.daam.fitmeet.data;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -29,6 +33,7 @@ public class FireBDatabase {
 
 
     }
+
 
     public DatabaseReference getDatabase() { return database;}
 
--- app/src/main/java/pt/iul/iscte/daam/fitmeet/firebase/FirebaseTestActivity.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/firebase/FirebaseTestActivity.java
@@ -8,8 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import pt.iul.iscte.daam.fitmeet.R;
 import pt.iul.iscte.daam.fitmeet.data.FireBDatabase;
+import pt.iul.iscte.daam.fitmeet.data.User;
 
 /**
  * Created by jdandrade on 25/03/2017.
@@ -18,6 +26,7 @@ import pt.iul.iscte.daam.fitmeet.data.FireBDatabase;
 public class FirebaseTestActivity extends AppCompatActivity {
 
   private Button button;
+    private DatabaseReference mUserReference;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -32,6 +41,33 @@ public class FirebaseTestActivity extends AppCompatActivity {
 
     setupViews();
     tabordenzTestingFirebase();
+
+      mUserReference = new FireBDatabase("Users").getDatabase();
+
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    ValueEventListener userListener = new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+          User user = dataSnapshot.child("1").getValue(User.class);
+          Toast.makeText(FirebaseTestActivity.this, user.getEmail() + " " + user.getName()+" ", Toast.LENGTH_SHORT).show();
+         if (user != null) {
+              System.out.println(user);
+          }
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+        System.out.println("loadUser:onCancelled " + databaseError.toException());
+
+        Toast.makeText(FirebaseTestActivity.this, "Failed to load user.",
+                Toast.LENGTH_SHORT).show();
+      }
+    };
+    mUserReference.addValueEventListener(userListener);
   }
 
   private void setupViews() {
@@ -51,7 +87,7 @@ public class FirebaseTestActivity extends AppCompatActivity {
         db.writeNewUser(1, "Bruno", "boss@b.pt","url");
         db.writeNewUser(2, "Bruno1", "boss@b.pt1","url1");
         //db.getDatabase().setValue("Hello world!");
-        //db.getDatabase().push();
+        //db.getDatabase().push()
 
         Toast.makeText(FirebaseTestActivity.this, "BOOP", Toast.LENGTH_SHORT).show();
       }
