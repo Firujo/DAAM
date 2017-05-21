006fcfc9221e99c38a9ade1211e0f58ecf32c33d
phillipsgoncalves <filisgoncalves@gmail.com>
2017-05-21 17:40:25 +0100

    Created SharedPreferencesUtils to hold all keys references

---
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/Utils/SharedPreferencesUtils.java
@@ -0,0 +1,13 @@
+package pt.iul.iscte.daam.fitmeet.Utils;
+
+/**
+ * Created by filipe on 21-05-2017.
+ */
+
+public class SharedPreferencesUtils {
+
+  public static final String SHARED_PREFERENCES_NAME = "pt.iul.daam.fitmeet";
+  public static final String LOGIN_EMAIL = "LOGIN_EMAIL";
+  public static final String LOGIN_PASSWORD = "LOGIN_PASSWORD";
+  public static final String LOGIN_STATUS = "LOGIN_STATUS";
+}
