6385afc3a4fbc09550be163567d0e0c1a2cbfe33
phillipsgoncalves <filisgoncalves@gmail.com>
2017-05-24 01:58:21 +0100

    Added logout method and refactored LoggedStatus listener

--- app/src/main/java/pt/iul/iscte/daam/fitmeet/account/LoginStatusManager.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/account/LoginStatusManager.java
@@ -24,12 +24,23 @@ public class LoginStatusManager {
 
   public void checkLoginStatus(AccountPresenter.LoginStatusListener listener) {
     boolean status = sharedPreferences.getBoolean(SharedPreferencesUtils.LOGIN_STATUS, false);
-    listener.isLoggedIn(status);
+    if (status) {
+      String name = sharedPreferences.getString(SharedPreferencesUtils.LOGIN_EMAIL, "");
+      listener.isLoggedIn(name);
+    } else {
+      listener.notLoggedIn();
+    }
   }
 
   public void saveLoginStatus(String username, String password) {
     sharedPreferences.edit().putString(SharedPreferencesUtils.LOGIN_EMAIL, username).apply();
     sharedPreferences.edit().putString(SharedPreferencesUtils.LOGIN_PASSWORD, password).apply();
     sharedPreferences.edit().putBoolean(SharedPreferencesUtils.LOGIN_STATUS, true).apply();
+  }
+
+  public void logout() {
+    sharedPreferences.edit().remove(SharedPreferencesUtils.LOGIN_STATUS).apply();
+    sharedPreferences.edit().remove(SharedPreferencesUtils.LOGIN_EMAIL).apply();
+    sharedPreferences.edit().remove(SharedPreferencesUtils.LOGIN_PASSWORD).apply();
   }
 }
