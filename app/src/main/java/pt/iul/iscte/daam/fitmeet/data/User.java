80ea34564c2115c247956a815cee8fa2b9599c21
jdandrade <j_diogo_@hotmail.com>
2017-05-27 15:38:56 +0100

    Codestyle.

--- app/src/main/java/pt/iul/iscte/daam/fitmeet/data/User.java
+++ app/src/main/java/pt/iul/iscte/daam/fitmeet/data/User.java
@@ -6,16 +6,16 @@ import com.google.firebase.database.IgnoreExtraProperties;
  * Created by jdandrade on 09/02/2017.
  */
 
-@IgnoreExtraProperties
-public class User {
+@IgnoreExtraProperties public class User {
   private long id;
   private String name;
   private String email;
   private String avatarUrl;
 
-    public User(){
-      // Default constructor required for calls to DataSnapshot.getValue(User.class)
-    }
+  public User() {
+    // Default constructor required for calls to DataSnapshot.getValue(User.class)
+  }
+
   public User(long id, String name, String email, String avatarUrl) {
     this.id = id;
     this.name = name;
